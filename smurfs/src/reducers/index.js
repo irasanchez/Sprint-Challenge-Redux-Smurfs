6b6fe34df12f2b40471f680d663c3fc397b0a3e1
iradsanchez <ira.d.sanchez@gmail.com>
2019-02-23 11:12:08 -0700

    Implement rootReducer

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -23,6 +23,7 @@ const initialState = {
   deletingSmurf: false,
   error: null
 };
+
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
@@ -30,3 +31,35 @@ const initialState = {
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+export const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.GETTING_SMURFS:
+      return { ...state, gettingSmurfs: true };
+    case actionTypes.GET_SMURFS:
+      return { ...state, smurfs: action.payload, gettingSmurfs: false };
+    case actionTypes.UPDATING_SMURF:
+      return { ...state, updatingSmurf: true };
+    case actionTypes.UPDATE_SMURF:
+      return { ...state, smurfs: action.payload, updatingSmurf: false };
+    case actionTypes.DELETING_SMURF:
+      return { ...state, deletingSmurf: true };
+    case actionTypes.DELETE_SMURF:
+      return { ...state, smurfs: action.payload, deletingSmurf: false };
+    case actionTypes.CREATING_SMURF:
+      return { ...state, creatingSmurf: true };
+    case actionTypes.CREATE_SMURF:
+      return { ...state, smurfs: action.payload, creatingSmurf: false };
+    case actionTypes.ERROR:
+      return {
+        ...state,
+        gettingSmurfs: false,
+        creatingSmurf: false,
+        deletingSmurf: false,
+        updatingSmurf: false,
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+};
--- smurfs/src/reducers/singleSmurfReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { friendsReducer } from "./friendsReducer";
-import { singleSmurfReducer } from "./singleFriendReducer";
-
-export default combineReducers({
-  smurfsReducer,
-  singleSmurfReducer
-});
